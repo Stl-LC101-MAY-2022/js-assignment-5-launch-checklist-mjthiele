612f170697d29d6590d287b5702fc036d3ff460e
mjthiele <matthewjthiele@gmail.com>
2022-07-20 15:28:16 -0500

    test

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+        let planetValues = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planetValues.name, planetValues.diameter, planetValues.star, planetValues.distance, planetValues.moons, planetValues.image);
+    })
    
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        
+    })
+
 });
